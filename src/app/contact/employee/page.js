2779ae501dae28f342351a1f5a84223c7c95dbae
Rishab Raj <rr0948555@gmail.com>
2026-08-21 10:17:35 +0530

    Nested Routing

---
+++ src/app/contact/employee/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react'
+
+const Employee = () => {
+  return (
+      <>
+        <h2>Contact Employee</h2>
+        <Link href='/contact'>Go Back</Link>
+    </>
+  )
+}
+
+export default Employee;
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,19 @@
+'use client';
+
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
+  let router = useRouter();
   return (
     <>
      <h2>Contact Page</h2>
-     <Link href='/home'>Home Page</Link>
+     {/* <Link href='/home'>Home Page</Link> */}
+     <Link href='/contact/employee'>Contact Employee</Link>
+     <br />
+     <br />
+     <button onClick={() => router.push('/contact/company')}>Contact Company</button>
     </>
   )
 }
