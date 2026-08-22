9b46067bd9829c6ab03f37a455a4e87db7fc46a1
Rishab Raj <rr0948555@gmail.com>
2026-08-22 10:26:19 +0530

    Page Not Found

---
+++ src/app/home/[...pagenotfound]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RouteBasedPageNotFound = () => {
+  return (
+    <> 
+       <h2>404 Page | Page Not Found For Home Route</h2>
+    </>
+  )
+}
+
+export default RouteBasedPageNotFound
