edc26412bba5cb0ed4fcf4b69fd8abf6ee46d2a9
Rishab Raj <rr0948555@gmail.com>
2026-08-26 10:58:11 +0530

    Post API With Static Data

---
+++ src/app/api/users/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server"
+
+
+export const POST =async (req, res) => {
+    let data = await req.json();
+    console.log(data);
+    if(!data.id || !data.salary){
+        return NextResponse.json({result: 'Bad Request'} , {status: 400})
+    }
+    // return NextResponse.json({name: "Alex", age: 45});
+    return NextResponse.json({result: 'Success'}, {status: 200})
+}
