29efc3d8428ad9b09979cce4c272b92a79bd5eab
Rishab Raj <rr0948555@gmail.com>
2026-08-27 09:57:03 +0530

    Delete API Method

--- src/app/api/employee/[empid]/route.js
+++ src/app/api/employee/[empid]/route.js
@@ -12,3 +12,14 @@ export const GET = async (req, { params }) => {
     let result = empData.length == 0 ? {result: 'No Employee Found'} : {result: empData}
     return NextResponse.json(result)
 }
+
+export const DELETE = async (req, { params }) => {
+
+    const { empid } = await  params;
+    let employeeId = empid;
+    if (employeeId) {
+        return NextResponse.json({result: "Employee Info Deleted Successfully"}, {status: 200})
+    } else {
+        return NextResponse.json({result: "Employee Not Found"}, {status: 404})
+    }
+}
