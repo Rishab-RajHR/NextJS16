63eafe2eee5f2cceb9250cf0a249172e996077b5
Rishab Raj <rr0948555@gmail.com>
2026-08-31 10:08:34 +0530

    PUT API Method

---
+++ src/app/api/db-emp/[empid]/route.js
@@ -0,0 +1,24 @@
+import { connectionString } from "@/lib/database/db_connection";
+import { Employee } from "@/lib/database/model/employee";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export const PUT = async (req, {params}) => {
+     // for id
+     const { empId } = await params;
+
+     const id = { _id: empId };
+    //  console.log(id);
+
+     // For Data
+     const payload = await req.json();
+    //  console.log(payload);
+
+    // For Mongo DB
+   await mongoose.connect(connectionString);
+
+   // getting result
+   const result = await Employee.findOneAndUpdate(id,payload)
+
+     return NextResponse.json({result, success: true})
+}
