8b9618bf570cea1f4f48aeb9889aba7a52ef5ed4
Rishab Raj <rr0948555@gmail.com>
2026-09-03 10:32:41 +0530

    Send and Display the UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,14 @@
 'use client';
 import Link from "next/link";
 import styles from "./page.module.css";
+import AddEmployees from "./components/AddEmployees";
+import ShowEmployees from "./components/ShowEmployees";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-        <main className={styles.main}>
-            <h2>Alex Pandian</h2>
-            {/* <EventsFunctionsState /> */}
-            <Link href='/mongo-add-emp'>Add Employee</Link> <br/> <br/>
-            <Link href='/mongo-get-emp'>Get Employee</Link>
-        </main>
-    </div>
+     <>
+        <AddEmployees />
+        <ShowEmployees />
+     </>
   );
 }
