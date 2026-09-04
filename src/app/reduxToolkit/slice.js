2f01342fc591030d5fcd9fae332178c82cffb859
Rishab Raj <rr0948555@gmail.com>
2026-09-04 10:17:58 +0530

    Removing the Data from  Redux Store

--- src/app/reduxToolkit/slice.js
+++ src/app/reduxToolkit/slice.js
@@ -9,14 +9,21 @@ const Slice = createSlice({
      initialState,
      reducers:{
         addEmployee:(state, action) => {
+            console.log(action);
              const data = {
                id:nanoid(),
                name:action.payload
              }
              state.employees.push(data)
+        },
+        removeEmployee:(state, action)=> {
+           const data = state.employees.filter((item) => {
+              return item.id !== action.payload
+           })
+           state.employees = data;
         }
      }
 });
 
-export const {addEmployee} = Slice.actions;
+export const {addEmployee, removeEmployee} = Slice.actions;
 export default Slice.reducer;
