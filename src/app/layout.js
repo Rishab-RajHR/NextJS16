1babf82722771ce1b0f9ce4b6d2c20bd06976ba0
Rishab Raj <rr0948555@gmail.com>
2026-09-04 10:00:00 +0530

    Adding Redux Tool Kit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./style.css";
+import Provider from "react-redux";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-     
+      <Provider>
       <body>
         {/* <h2>My Name is Alex</h2> */}
-        {children}</body>
+        {children}
+        </body>
+        </Provider>
     </html>
   );
 }
